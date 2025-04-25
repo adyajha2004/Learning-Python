dc080d3f51f89b978f4107c812fa1af5f0eb618a
Adya <138201657+adyajha2004@users.noreply.github.com>
2025-04-25 14:16:21 +0000

    update with time delay

--- GuessTheNum.py
+++ GuessTheNum.py
@@ -1,21 +1,33 @@
 import random
+import time
 
 # Instruction
-print("Welcome to Number Guesser! \nThe computer will choose a number and if you choose the same, You WIN")
+print("Welcome to Number Guesser!")
+time.sleep(0.5)
+print("The computer will choose a number and if you choose the same, You WIN")
+time.sleep(2)
+
+# Game start
+print("Let's begin...")
 n = int(input("select the range: "))
 
 if (n < 3): # restriction
+    time.sleep(2)
     print("there's not a lot left to guess. is it?")
 
 #game code
 else:
     Comp_num = random.randrange(1,n)
+    time.sleep(2)
     print("Computer succesfully selected the number...")
-    Human_num = int(input("Enter the number you think the computer choose? \n"))
+    Human_num = int(input("Enter the number you think the computer choose? "))
 
     if (Comp_num == Human_num):
+        time.sleep(2)
         print("Yay!! you WON 🎉")
     else:
-        print("the comuter picked:", Comp_num)
-        print("but you choose:", Human_num)
+        print("you choose:", Human_num)
+        time.sleep(1)
+        print("But comuter picked:", Comp_num)
+        time.sleep(1)
         print("Better luck next time")
